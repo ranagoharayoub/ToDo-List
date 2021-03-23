4c895c6c10f1dabbe2ffce0eb64ce48be4677075
ranagoharayoub <ranagohar1163@gmail.com>
2021-03-23 17:24:22 +0500

    Modal Modefied

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,15 @@
 
-import { Checkbox } from '@material-ui/core'
+import { Checkbox, FormControlLabel } from '@material-ui/core'
 import React from 'react'
 import { Button, Modal, Form } from 'react-bootstrap'
-
+import './AddTask.css'
 
 function AddTask(props) {
     return (
-
+      <div >
     <Modal
       {...props}
-      size="md"
+      size="sm"
       aria-labelledby="contained-modal-title-vcenter"
       centered = 'true'
     >
@@ -20,16 +20,24 @@ function AddTask(props) {
       </Modal.Header>
       <Modal.Body>
       <Form.Control type="text" placeholder="Type here...." />
-      <div style={{display: 'flex', alignItems: 'center'}}>
-        <Checkbox color='primary'></Checkbox>
-        Mark as Priority Task
+      <div style={{display: 'flex', alignItems: 'center', marginTop: '10px'}}>
+        {/* <Checkbox color='primary'></Checkbox>
+        Mark as Priority Task */}
+        <FormControlLabel
+          value="end"
+          control={<Checkbox size='small' color="primary" />}
+          label={<span style={{ fontSize: 'small' }}>Mark as Priority Task</span>}
+          labelPlacement="end"
+          style={{fontSize: 'small'}}
+        />
       </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button>Create</Button>
-        <Button variant='danger' onClick={props.onHide}>Cancel</Button>
+      <Modal.Footer style={{marginBottom: '20px'}}>
+        <Button style={{marginRight: '20px', padding: '0px 35px'}} >Create</Button>
+        <Button style={{padding: '0px 35px'}} variant="light" onClick={props.onHide}>Cancel</Button>
       </Modal.Footer>
     </Modal>
+    </div>
 
     )
 }
